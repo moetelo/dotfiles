6cbd79ad7ec5e26c5f2867bb42fb863f2a8717a6
Mikhail Gunin <gunka462@gmail.com>
2025-05-08 00:00:21 +0300

    st-swapmouse

--- .config/st/x.c
+++ .config/st/x.c
@@ -261,6 +261,9 @@ static char *opt_title = NULL;
 static uint buttons; /* bit field of pressed buttons */
 static int cursorblinks = 0;
 
+static Cursor cursor;
+static XColor xmousefg, xmousebg;
+
 void
 clipcopy(const Arg *dummy)
 {
@@ -1154,10 +1157,10 @@ void
 xinit(int cols, int rows)
 {
 	XGCValues gcvalues;
-	Cursor cursor;
+	// Cursor cursor;
 	Window parent, root;
 	pid_t thispid = getpid();
-	XColor xmousefg, xmousebg;
+	// XColor xmousefg, xmousebg;
 	XWindowAttributes attr;
 	XVisualInfo vis;
 
@@ -1789,6 +1792,14 @@ xsetmode(int set, unsigned int flags)
 {
 	int mode = win.mode;
 	MODBIT(win.mode, set, flags);
+
+	if (flags & MODE_MOUSE) {
+		if (win.mode & MODE_MOUSE)
+			XUndefineCursor(xw.dpy, xw.win);
+		else
+			XDefineCursor(xw.dpy, xw.win, cursor);
+	}
+
 	if ((win.mode & MODE_REVERSE) != (mode & MODE_REVERSE))
 		redraw();
 }
